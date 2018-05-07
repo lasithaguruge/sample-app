2f43f547e24f5980cd09416dfd3a034719b37ae1
lasithaguruge <lasithaguruge91@gmail.com>
2018-05-07 08:08:13 +0530

    Add 3 new files called Header.js, Main.js and Footer.js

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HERE'S THE HEADER CONTENT</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
