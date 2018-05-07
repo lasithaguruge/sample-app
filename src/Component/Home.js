86b7d47aa974456c748d8680744e37ad38a4bef2
lasithaguruge <lasithaguruge91@gmail.com>
2018-05-07 10:20:08 +0530

    Add a home component

---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div style={{minHeight: '350px'}}>
+                <h1>Welcome to home page!!!</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
